be12e7b0370c332a4ecdf9622be3fc8171c9d712
Administrator <Administrator@10PC315>
2024-03-22 15:28:03 +0700

    Add project files.

---
+++ BTTuan3/Models/ICategoryReposity.cs
@@ -0,0 +1,11 @@
+﻿namespace BTTuan3.Models
+{
+    public interface ICategoryReposity
+    {
+        Task<IEnumerable<Category>> GetAllAsync();
+        Task<Category> GetByIdAsync(int id);
+        Task AddAsync(Category category);
+        Task UpdateAsync(Category category);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ BTTuan3/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BTTuan3.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required, StringLength(100)]
+        public string Name { get; set; }
+        [Range(0.01, 10000.00)]
+        public int Price { get; set; }
+        public string Description { get; set; }
+        public string? ImageUrl { get; set; }
+        public List<ProductImage>? Images { get; set; }
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+    }
+}
